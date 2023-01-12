363ab92bf52f704d9465f31be46bf4b84e2ae943
Mark <niujl123@sina.com>
2023-01-12 13:46:39 +0800

    remove klipper to other github

--- klipper20221225/src/generic/timer_irq.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GENERIC_TIMER_IRQ_H
-#define __GENERIC_TIMER_IRQ_H
-
-uint32_t timer_dispatch_many(void);
-
-#endif // timer_irq.h
--- klipper20221225/src/stepper.h
+++
@@ -1,8 +0,0 @@
-#ifndef __STEPPER_H
-#define __STEPPER_H
-
-#include <stdint.h> // uint8_t
-
-uint_fast8_t stepper_event(struct timer *t);
-
-#endif // stepper.h
