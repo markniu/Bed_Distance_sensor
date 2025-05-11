46cafd06e1a8bb675e4dd602986d630afa76d4ed
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2025-05-11 14:30:02 +0200

    Allow axis_twist_compensation to update results during rapid scan

--- klipper/BDsensor.py
+++ klipper/BDsensor.py
@@ -364,6 +364,8 @@ class BDPrinterProbe:
                     pos[2] = pos[2] - intd + self.mcu_probe.endstop_bdsensor_offset
                     self.mcu_probe.results.append(pos)
                     if len(self.mcu_probe.results) < 500:
+                        # Allow axis_twist_compensation to update results
+                        self.printer.send_event("probe:update_results", pos)
                         self.gcode.respond_info("probe at %.3f,%.3f is z=%.6f"
                                             % (pos[0], pos[1], pos[2]))
                     break
